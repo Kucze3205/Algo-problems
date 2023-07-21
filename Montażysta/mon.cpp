17e253180fe0242a17c2d6234cf1719ad2955ecf
Kucze3205 <kuczerowski2005@gmail.com>
2023-07-21 20:11:22 +0200

    montażysta fix

--- Montażysta/mon.cpp
+++ Montażysta/mon.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-priority_queue< pair<long, long>> seq;
-pair<long, long> res[500002];
+priority_queue< pair<long, long>> seq; //termin publikacji, czas montowania
+priority_queue<pair<long, int>> result; //czas montowania, numer filmu
 
 int main()
 {
@@ -22,27 +22,43 @@ int main()
         seq.emplace(-b, a);
     }
 
-    pair<int, int> miejsce = { 0,-seq.top().first };
+    pair<int, int> miejsce = { 0,-seq.top().first }; //pocz¹tek miejsca, wielkoœæ miejsca
 
     for (int i = 1; i <= n; i++) {
 
         if (seq.top().second <= miejsce.second) {
-            res[m] = { i, miejsce.first };
-            m++;
             miejsce.first += seq.top().second;
             miejsce.second -= seq.top().second;
+            result.push({ seq.top().second,i });
+        }
+        else if (!result.empty() && result.top().first > seq.top().second) {
+            miejsce.first += -result.top().first + seq.top().second;
+            miejsce.second += seq.top().second - seq.top().second;
+            result.push({ seq.top().second,i });
+            result.pop();
+
         }
 
         seq.pop();
         miejsce.second = -seq.top().first - miejsce.first;
     }
+    priority_queue<pair<int, long>> resultNum; // numer w tablicy, czas montowania
 
-    cout << m << "\n";
-    for (int i = 0; i < m; i++) {
-        cout << res[i].first;
+    while (!result.empty()) {
+        resultNum.push({ -result.top().second,result.top().first });
+        result.pop();
+    }
+    cout << resultNum.size() << "\n";
+
+    int rozpoczecie = 1;
+
+    while (!resultNum.empty()) {
+        cout << -resultNum.top().first;
         cout << " ";
-        cout << res[i].second + 1;
+        cout << rozpoczecie;
+        rozpoczecie += resultNum.top().second;
         cout << "\n";
+        resultNum.pop();
     }
 
 }
